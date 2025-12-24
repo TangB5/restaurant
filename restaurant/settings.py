15e1ab86e471119bb16d77b254966517c8093f8b
KingTang <ndohyannick78@gmail.com>
2025-12-24 11:11:13 +0100

    mise a jour du vercel.json 1.12

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -33,8 +33,9 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # 'jet.dashboard',
     # 'jet',
+    # 'simpleui',
     'whitenoise.runserver_nostatic',
-    'simpleui',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
